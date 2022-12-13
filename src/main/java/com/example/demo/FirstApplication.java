ac4892e3febb238f1d65af9360bc535dce540c7c
pranavesh8 <105205641+pranavesh8@users.noreply.github.com>
2022-12-13 16:30:14 +0530

    Update FirstApplication.java

--- src/main/java/com/example/demo/FirstApplication.java
+++ src/main/java/com/example/demo/FirstApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class FirstApplication {
@@ -9,5 +10,9 @@ public class FirstApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FirstApplication.class, args);
 	}
+	
+	 protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	      return builder.sources(First1Application.class);
+	  }
 
 }
