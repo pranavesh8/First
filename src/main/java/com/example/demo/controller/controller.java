6fba9541c6c4b9ea1d017662a5d23d6d53c2aca4
Pranavesh Mara <pranavesh.mara@ratnaglobaltech.com>
2022-12-13 10:36:38 +0530

    commit

---
+++ src/main/java/com/example/demo/controller/controller.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+
+	@GetMapping("/welcome/{name}")
+	public String welcome(@PathVariable("name") String name) {
+		return name + ", Welcome to  RGT Family..!!";
+
+	}
+
+}
