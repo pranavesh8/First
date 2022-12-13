e698ed80b680138e227d84d99d2234c0b80ee1e6
pranavesh8 <105205641+pranavesh8@users.noreply.github.com>
2022-12-13 11:04:21 +0530

    Update FirstApplicationTests.java

--- src/test/java/com/example/demo/FirstApplicationTests.java
+++ src/test/java/com/example/demo/FirstApplicationTests.java
@@ -1,8 +1,8 @@
 package com.example.demo;
-
+import org.junit.runner.RunWith;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class FirstApplicationTests {
 
