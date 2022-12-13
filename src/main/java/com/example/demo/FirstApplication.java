e95a88096367ca550c3acbcabc7ae87d2de6d507
pranavesh8 <105205641+pranavesh8@users.noreply.github.com>
2022-12-13 16:39:47 +0530

    Update FirstApplication.java

--- src/main/java/com/example/demo/FirstApplication.java
+++ src/main/java/com/example/demo/FirstApplication.java
@@ -11,16 +11,16 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class First1Application extends SpringBootServletInitializer{
+public class FirstApplication extends SpringBootServletInitializer{
 
 	
 	 @Override
 	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-	        return application.sources(First1Application.class);
+	        return application.sources(FirstApplication.class);
 	    }
 	
 	public static void main(String[] args) {
-		SpringApplication.run(First1Application.class, args);
+		SpringApplication.run(FirstApplication.class, args);
 	}
 
 	 
